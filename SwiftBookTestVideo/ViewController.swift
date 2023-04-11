b805851076d6426d8edabce9f6be43f9575ccccf
nikaDo <oohnorthregion@gmail.com>
2023-04-11 21:29:14 +0400

    Initial Commit

---
+++ SwiftBookTestVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookTestVideo
+//
+//  Created by коля on 11.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
